6ef78e2c61d1d6a813b73694bc7f7acde5aea0eb
maraf <80293557+maraf12@users.noreply.github.com>
2025-05-25 20:12:29 +0100

    cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const port = 3000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("🚀 Hello from AKS Node.js App!");
-}).listen(port, () => console.log(`Server running on port ${port}`));
